76ef48bc6fef478d0de95acf9944c207bbbdb7ee
Hani-hue <isnahabibah8729@gmail.com>
2025-03-23 22:36:39 +0700

    tampilan susunan data element

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -10,5 +10,19 @@ void input()
     {
         cout << "Masukkan Jumlah Data pada Array : "; // Membuat Inputan jumlah element Array
         cin >> n;                                     // memanggil variable inputan n
+
+        if (n <= 20)
+        { // Membuat kondisi n tidak lebih dari 20
+            break;
+        }
+        else
+        {
+            cout << "nArray yang anda masukkan maksimal 20 elemen. \n"; // Menampilkan pesan jika data lebih dari 20
+        }
     }
+    cout << endl;                             // Membuat jarak per baris program
+    cout << "======================" << endl; // Membuat tampilan sususan data element array
+    cout << "Masukkan Element Array" << endl;
+    cout << "======================" << endl;
+}
 }
